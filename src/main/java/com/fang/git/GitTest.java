c516a03174f79606110015d2600a93c7b6c98ac5
fang <fang@123.com>
2021-05-08 20:56:57 +0800

    push commit

--- src/main/java/com/fang/git/GitTest.java
+++ src/main/java/com/fang/git/GitTest.java
@@ -7,5 +7,6 @@ public class GitTest {
         System.out.println("hello git2");
         System.out.println("222");
         System.out.println("111");
+        System.out.println("111");
     }
 }
